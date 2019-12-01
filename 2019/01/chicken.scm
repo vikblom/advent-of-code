c1edaed0855896a3cedeb78cf1d08b758a9f737e
Viktor Blomqvist <veblomqvist@gmail.com>
2019-12-01 10:07:14 +0100

    Day 1 Chicken Scheme.

---
+++ 2019/01/chicken.scm
@@ -0,0 +1,26 @@
+;; Input
+(with-input-from-file path
+  (lambda () (let loop
+                 ((count 0))
+               (if (eof-object? (read-line)) count (loop (+ count 1))))))
+
+(define masses
+  (map string->number
+       (call-with-input-file "input.txt"
+         (lambda (port) (read-lines port)))))
+
+;; Part 1
+(define (fuel-req mass) (-  (floor (/ mass 3)) 2))
+
+(fold + 0 (map (lambda (mass) (fuel-req mass)) masses))
+
+
+;; Part 2
+(define (total-fuel mass)
+  (define (fuel-fuel acc fuel)
+    (if (<= fuel 0)
+        acc
+        (fuel-fuel (+ acc fuel) (fuel-req fuel))))
+  (fuel-fuel 0 (fuel-req mass)))
+
+(fold + 0 (map (lambda (mass) (total-fuel mass)) masses))
