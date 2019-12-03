f399e795b494d2f77ac7232e0623a0686c15f139
Viktor Blomqvist <veblomqvist@gmail.com>
2019-12-03 18:28:31 +0100

    Switched to a hashtable, much faster.

--- 2019/03/chicken.scm
+++ 2019/03/chicken.scm
@@ -1,11 +1,13 @@
 ;; INPUT
-
-(import (chicken io) (chicken string) srfi-1)
+(import (chicken io) (chicken string) srfi-1 srfi-69)
 
 (define input
   (call-with-input-file "input.txt"
     (lambda (port) (read-lines port))))
 
+(define (parse-move raw)
+  (let ((in  (string-split raw " ")))
+    (cons (car in) (string->number (cadr in)))))
 
 (define (parse-moves raw)
   (map parse-move
@@ -16,11 +18,6 @@
                              ("D" . "down ")
                              ("L" . "left ")))
         ",")))
-
-(define (parse-move raw)
-  (let ((in  (string-split raw " ")))
-    (cons (car in) (string->number (cadr in)))))
-
 
 (define (right pos) (cons (+ (car pos) 1) (cdr pos)))
 (define (left pos) (cons (- (car pos) 1) (cdr pos)))
@@ -37,26 +34,41 @@
 (define (trailer instr sofar)
   (append (trail (car sofar) (car instr) (cdr instr)) sofar))
 
-
-
-(define (pair-eq? a b)
-  (and (eq? (car a) (car b)) (eq? (cdr a) (cdr b))))
 (define (manhattan pos) (+ (abs (car pos)) (abs (cdr pos))))
-
 
 (define (str-to-pos input)
   (cdr (reverse
         (fold trailer '((0 . 0))
               (parse-moves input)))))
 
-(define (crossings in1 in2)
-  (lset-intersection pair-eq? in1 in2))
+(define (xs pos1 pos2)
+  (let ((pmap (make-hash-table)))
+    (begin
+      (map (lambda (p) (hash-table-set! pmap p 1))
+           pos1)
+      (filter (lambda (p) (hash-table-exists? pmap p))
+              pos2))))
+
+(define (closest-cross in1 in2)
+  (apply min (map manhattan (xs in1 in2))))
 
 
-(define (closest-intersect a b)
-  (map manhattan intersects))
+(define (part-one str1 str2)
+  (let ((pos1 (str-to-pos str1))
+        (pos2 (str-to-pos str2)))
+    (closest-cross pos1 pos2)))
 
-;;(print (apply min (dist-to-cross (car input) (cadr input))))
+
+(part-one "R8,U5,L5,D3" "U7,R6,D4,L4") ;; 6
+
+(part-one "R75,D30,R83,U83,L12,D49,R71,U7,L72"
+          "U62,R66,U55,R34,D71,R55,D58,R83") ;; 159
+
+(part-one "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
+          "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7") ;; 135
+
+(print (part-one (car input) (cadr input)))
+
 
 ;; PART 2
 ;; We get one step less for each wire because (0 . 0) is
@@ -67,22 +79,22 @@
          (map +
               (map (lambda (cross)
                      (list-index (lambda (x)
-                                   (pair-eq? x cross)) in1)) crosses)
+                                   (equal? x cross)) in1)) crosses)
               (map (lambda (cross)
                      (list-index (lambda (x)
-                                   (pair-eq? x cross)) in2)) crosses))))
+                                   (equal? x cross)) in2)) crosses))))
 
 (define (part-two str1 str2)
   (let ((pos1 (str-to-pos str1))
         (pos2 (str-to-pos str2)))
-    (+ 2 (best-cross (crossings pos1 pos2) pos1 pos2))))
+    (+ 2 (best-cross (xs pos1 pos2) pos1 pos2))))
 
-(part-two "R8,U5,L5,D3" "U7,R6,D4,L4")
+(part-two "R8,U5,L5,D3" "U7,R6,D4,L4") ;; 30
 
 (part-two "R75,D30,R83,U83,L12,D49,R71,U7,L72"
-          "U62,R66,U55,R34,D71,R55,D58,R83")
+          "U62,R66,U55,R34,D71,R55,D58,R83") ;; 610
 
 (part-two "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
-          "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")
+          "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7") ;; 410
 
 (print (part-two (car input) (cadr input)))
