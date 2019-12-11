b37a4d239bcd861e9716256c22c052f9a7caab9d
Viktor Blomqvist <veblomqvist@gmail.com>
2019-12-11 18:28:24 +0100

    Day 11 Chicken Scheme.

--- 2019/11/chicken.scm
+++ 2019/11/chicken.scm
@@ -30,14 +30,41 @@
         ;; Then move
         (move-n-paint paint npos ndir (cddr instr)))))
 
-(define (part-1 src paint)
+(define (run-robot src paint start)
   (let loop ((ic (intcode-run src))
-             (pos '(0 . 0))
+             (pos start)
              (dir 0))
     (define-values (npos ndir)
       (apply values
              (move-n-paint paint pos dir (intcode-pop-outputs! ic))))
     (if (intcode-halted? ic)
-        (print (hash-table-size paint))
+        paint
         (loop (intcode-run ic (hash-table-ref/default paint npos 0))
               npos ndir))))
+
+;; PART 1
+(print (run-robot input (make-hash-table) '(0 . 0)))
+
+
+(begin
+  (define two (make-hash-table))
+  (define start '(0 . 0))
+  (hash-table-set! two start 1)
+  (run-robot input two start)
+  (define painted-ps (hash-table-keys two))
+  (define xs (map car painted-ps))
+  (define ys (map cdr painted-ps))
+
+  (define low-x (apply min xs))
+  (define high-x (apply max xs))
+  (define low-y (apply min ys))
+  (define high-y (apply max ys))
+
+  (for-each (lambda (i)
+              (for-each (lambda (j)
+                          (print* (if (zero? (hash-table-ref/default two (cons i j) 0))
+                                      " "
+                                      "X")))
+                        (iota (+ 1 (- high-y low-y)) low-y))
+              (newline))
+            (reverse (iota (+ 1 (- high-x low-x)) low-x))))
