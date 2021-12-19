adaf768187e45aac95b3f5321f50831859e8c74f
Viktor Blomqvist <veblomqvist@gmail.com>
2021-12-19 11:25:23 +0100

    Make mkday.go a little easier to read.

--- 2021/mkday.go
+++ 2021/mkday.go
@@ -19,6 +19,60 @@ const YEAR = "2021"
 //go:embed template.txt
 var template string
 
+// aocGetRequest puts in the correct cookies to talk with AoC.
+func aocGetRequest(path, session string) (*http.Response, error) {
+
+	url := &url.URL{
+		Scheme: "https",
+		Host:   "adventofcode.com",
+		Path:   path,
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+	req.AddCookie(&http.Cookie{Name: "session", Value: session})
+
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		return nil, err
+	}
+	client := http.Client{
+		Jar: jar,
+	}
+	return client.Do(req)
+}
+
+func WriteInput(day, session string) error {
+
+	resp, err := aocGetRequest(path.Join(YEAR, "day", day, "input"), session)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Dump to input.txt
+	input, err := os.Create(path.Join(day, "input.txt"))
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(input, resp.Body)
+	if err != nil {
+		return err
+	}
+	err = input.Sync()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func WriteSolutionTemplate(path string) error {
+	return os.WriteFile(path, []byte(template), 0644)
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("usage: mkday day")
@@ -26,9 +80,9 @@ func main() {
 	}
 	day := os.Args[1]
 
-	token := os.Getenv("AOCCOOKIE")
-	if token == "" {
-		fmt.Println("Set AOCCOOKIE in env.")
+	session := os.Getenv("AOCSESSION")
+	if session == "" {
+		fmt.Println("Set AOCSESSION in env.")
 		os.Exit(1)
 	}
 
@@ -38,53 +92,13 @@ func main() {
 	}
 
 	// Template .go file
-	solve := path.Join(day, "solve_test.go")
-	if err := os.WriteFile(solve, []byte(template), 0644); err != nil {
-		log.Fatal(err)
-	}
-
-	// Request the input.
-	cookie := &http.Cookie{
-		Name:  "session",
-		Value: token,
-	}
-
-	url := &url.URL{
-		Scheme: "https",
-		Host:   "adventofcode.com",
-		Path:   path.Join(YEAR, "day", day, "input"),
-	}
-
-	req, err := http.NewRequest(http.MethodGet, url.String(), nil)
+	err := WriteSolutionTemplate(path.Join(day, "solve_test.go"))
 	if err != nil {
 		log.Fatal(err)
 	}
-	req.AddCookie(cookie)
 
-	jar, err := cookiejar.New(nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	client := http.Client{
-		Jar: jar,
-	}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-
-	// Dump to input.txt
-	input, err := os.Create(path.Join(day, "input.txt"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	_, err = io.Copy(input, resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = input.Sync()
+	// Get the input
+	err = WriteInput(day, session)
 	if err != nil {
 		log.Fatal(err)
 	}
