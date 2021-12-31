fbad23b25c711c2cf5d55312f8d5de8a438d28bf
Viktor Blomqvist <veblomqvist@gmail.com>
2021-12-31 18:30:49 +0100

    TMP2

--- 2021/23/solve_test.go
+++ 2021/23/solve_test.go
@@ -13,26 +13,29 @@ import (
 	aoc "gitlab.com/vikblom/advent-of-code"
 )
 
-const (
-	WIDTH  = 13
-	HEIGHT = 5
-)
-
-var energy = map[rune]int{
+var energy = map[int]int{
 	'A': 1,
 	'B': 10,
 	'C': 100,
 	'D': 1000,
 }
 
-var homecol = map[rune]int{
+var homecol = map[int]int{
 	'A': 3,
 	'B': 5,
 	'C': 7,
 	'D': 9,
 }
 
-func ReadMap(file string) (Map, error) {
+type Map struct {
+	rows int
+	cols int
+	data []int // row major
+}
+
+type Pods map[[2]int]int
+
+func ReadMap(file string, height, width int) (Map, Pods, error) {
 	fh, err := os.Open(file)
 	if err != nil {
 		log.Fatal(err)
@@ -40,7 +43,8 @@ func ReadMap(file string) (Map, error) {
 	defer fh.Close()
 	buf := bufio.NewReader(fh)
 
-	mat := NewMap(HEIGHT, WIDTH)
+	pods := make(Pods)
+	mat := NewMap(height, width)
 	row := 0
 	col := 0
 	for err == nil {
@@ -49,7 +53,7 @@ func ReadMap(file string) (Map, error) {
 			if err == io.EOF {
 				break
 			} else {
-				return mat, err
+				return mat, pods, err
 			}
 		}
 
@@ -57,17 +61,16 @@ func ReadMap(file string) (Map, error) {
 			col = 0
 			row++
 		} else {
-			mat.Set(row, col, int(b))
+			if 'A' <= b && b <= 'Z' {
+				pods[[2]int{row, col}] = int(b)
+				mat.Set(row, col, '.')
+			} else {
+				mat.Set(row, col, int(b))
+			}
 			col++
 		}
 	}
-	return mat, nil
-}
-
-type Map struct {
-	rows int
-	cols int
-	data []int // row major
+	return mat, pods, nil
 }
 
 func NewMap(rows, cols int) Map {
@@ -78,13 +81,17 @@ func NewMap(rows, cols int) Map {
 	}
 }
 
-func (m Map) String() string {
+func Draw(m Map, p Pods) string {
 	var s strings.Builder
 	for row := 0; row < m.rows; row++ {
 		for col := 0; col < m.cols; col++ {
-			c := m.At(row, col)
-			if c > 0 {
+			if c, ok := p[[2]int{row, col}]; ok {
 				fmt.Fprintf(&s, "%c", c)
+			} else {
+				c := m.At(row, col)
+				if c > 0 {
+					fmt.Fprintf(&s, "%c", c)
+				}
 			}
 		}
 		if row != m.rows-1 {
@@ -104,11 +111,18 @@ func (m Map) Set(row, col, val int) {
 	m.data[index] = val
 }
 
-func (m Map) Done() bool {
-	return m.At(2, 3) == 'A' && m.At(3, 3) == 'A' &&
-		m.At(2, 5) == 'B' && m.At(3, 5) == 'B' &&
-		m.At(2, 7) == 'C' && m.At(3, 7) == 'C' &&
-		m.At(2, 9) == 'D' && m.At(3, 9) == 'D'
+func (p Pods) Done() bool {
+	for k, v := range p {
+		row := k[0]
+		col := k[1]
+		if row < 2 {
+			return false
+		}
+		if col != homecol[v] {
+			return false
+		}
+	}
+	return true
 }
 
 func (m Map) Equal(other Map) bool {
@@ -123,25 +137,25 @@ func (m Map) Equal(other Map) bool {
 	return true
 }
 
-func (m Map) Copy() Map {
-	return Map{
-		rows: m.rows,
-		cols: m.cols,
-		data: append([]int{}, m.data...),
+func (p Pods) Copy() Pods {
+	new := make(Pods)
+	for k, v := range p {
+		new[k] = v
 	}
+	return new
 }
 
-func (s State) AllMovesFrom(row, col int) []*State {
-	c := s.m.At(row, col)
+func (s State) AllMovesFrom(m Map, row, col int) []*State {
+	c := s.pods[[2]int{row, col}]
 	if row == 1 { // corridor
-		home := homecol[rune(c)]
+		home := homecol[c]
 		// There must be place to move in
-		if s.m.At(2, home) != '.' {
+		if _, busy := s.pods[[2]int{2, home}]; busy {
 			return nil
 		}
 		// Can only move in with friends or empty
-		nbr := s.m.At(3, home)
-		if !(nbr == c || nbr == '.') {
+		nbr, busy := s.pods[[2]int{3, home}]
+		if busy && nbr != c {
 			return nil
 		}
 		// Check if the corridor is free, note that no ant can be at
@@ -150,7 +164,7 @@ func (s State) AllMovesFrom(row, col int) []*State {
 		right := aoc.MaxInt(col, home)
 		clear := true
 		for i := left + 1; i < right; i++ {
-			if s.m.At(1, i) != '.' {
+			if _, busy := s.pods[[2]int{1, i}]; busy {
 				clear = false
 				break
 			}
@@ -158,78 +172,69 @@ func (s State) AllMovesFrom(row, col int) []*State {
 		if !clear {
 			return nil
 		}
-		// Actually move.
+		// Actually move, figure out how deep we should place us.
 		d := 2
-		if nbr == '.' {
-			d = 3
+		for s.pods[[2]int{d + 1, home}] == 0 && d < m.rows-2 {
+			d++
 		}
-		e := energy[rune(c)] * (aoc.AbsInt(col-home) + aoc.AbsInt(row-d))
-		m := s.m.Copy()
-		m.Set(row, col, '.')
-		m.Set(d, home, c)
 
+		e := energy[c] * (aoc.AbsInt(col-home) + aoc.AbsInt(row-d))
+		next := s.pods.Copy()
+		delete(next, [2]int{row, col})
+		next[[2]int{d, home}] = c
 		return []*State{
 			{
 				energy: s.energy + e,
-				m:      m,
+				pods:   next,
 			},
 		}
 
 	} else {
-		// If this is deep in correct col,
-		// or whoever is deeper is also correct, dont move.
-		if col == homecol[rune(c)] && (row == 3 || s.m.At(3, col) == c) {
-			//log.Println("ALREADY GOOD")
-		} else if row == 3 && s.m.At(2, col) != '.' {
+		// Check if column is already packed with correct ants.
+		if col == homecol[c] {
+			if row == 3 || s.pods[[2]int{3, col}] == c {
+				//log.Println("ALREADY GOOD")
+				return nil
+			}
+		}
+
+		// Check if this and is blocked in by some other ant.
+		if row == 3 && s.pods[[2]int{2, col}] > 0 {
 			// Some other ant must get out first.
 			//log.Println("LOCKED IN")
-		} else {
-			// All possible moves into corridor
-			left := col
-			for s.m.At(1, left) == '.' {
-				left--
-			}
-			right := col
-			for s.m.At(1, right) == '.' {
-				right++
-			}
-
-			news := []*State{}
-			for i := left + 1; i < right; i++ {
-				if i == 3 || i == 5 || i == 7 || i == 9 {
-					continue // cannot block entrances
-				}
-				e := energy[rune(c)] * (aoc.AbsInt(col-i) + aoc.AbsInt(row-1))
-				m := s.m.Copy()
-				m.Set(row, col, '.')
-				m.Set(1, i, c)
-				new := &State{
-					energy: s.energy + e,
-					m:      m,
-				}
-				news = append(news, new)
-			}
-			return news
+			return nil
 		}
-	}
-	return nil
-}
 
-func (s *State) Moves() []*State {
-	moves := []*State{}
-	for row := 0; row < s.m.rows; row++ {
-		for col := 0; col < s.m.cols; col++ {
-			c := s.m.At(row, col)
-			if 'A' <= c && c <= 'D' {
-				moves = append(moves, s.AllMovesFrom(row, col)...)
-			}
+		// All possible moves into corridor
+		left := col
+		for s.pods[[2]int{1, left}] == 0 && left > 0 {
+			left--
 		}
+		right := col
+		for s.pods[[2]int{1, right}] == 0 && right < m.cols-1 {
+			right++
+		}
+
+		news := []*State{}
+		for i := left + 1; i < right; i++ {
+			if i == 3 || i == 5 || i == 7 || i == 9 {
+				continue // cannot block entrances
+			}
+			e := energy[c] * (aoc.AbsInt(col-i) + aoc.AbsInt(row-1))
+			next := s.pods.Copy()
+			delete(next, [2]int{row, col})
+			next[[2]int{1, i}] = c
+			news = append(news, &State{
+				energy: s.energy + e,
+				pods:   next,
+			})
+		}
+		return news
 	}
-	return moves
 }
 
 type State struct {
-	m      Map
+	pods   Pods
 	energy int
 
 	// index is needed for heap update
@@ -264,15 +269,34 @@ func (q *Queue) Pop() interface{} {
 	return item
 }
 
+// func TestFoo(t *testing.T) {
+// 	m, pods, err := ReadMap("foo.txt", 5, 13)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+
+// 	head := State{
+// 		energy: 0,
+// 		pods:   pods,
+// 	}
+
+// 	//for pos := range pods {
+// 	//for _, st := range head.AllMovesFrom(m, pos[0], pos[1]) {
+// 	for _, st := range head.AllMovesFrom(m, 1, 11) {
+// 		fmt.Println(Draw(m, st.pods))
+// 	}
+// 	//}
+// }
+
 func TestPartOne(t *testing.T) {
-	m, err := ReadMap("input.txt")
+	m, pods, err := ReadMap("input.txt", 5, 13)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	q := make(Queue, 1)
 	q[0] = &State{
-		m:      m,
+		pods:   pods,
 		energy: 0,
 	}
 
@@ -282,19 +306,21 @@ func TestPartOne(t *testing.T) {
 	for {
 		head = heap.Pop(&q).(*State)
 
-		s := head.m.String()
-		if seen[s] {
+		str := Draw(m, head.pods)
+		if seen[str] {
 			continue
 		}
-		seen[s] = true
+		seen[str] = true
 
-		if head.m.Done() {
+		if head.pods.Done() {
 			ans = head.energy
 			break
 		}
 
-		for _, m := range head.Moves() {
-			heap.Push(&q, m)
+		for pos := range head.pods {
+			for _, st := range head.AllMovesFrom(m, pos[0], pos[1]) {
+				heap.Push(&q, st)
+			}
 		}
 	}
 
@@ -302,11 +328,12 @@ func TestPartOne(t *testing.T) {
 }
 
 func TestPartTwo(t *testing.T) {
-	input, err := ReadMap("test.txt")
+	input, pods, err := ReadMap("test2.txt", 7, 13)
 	if err != nil {
 		t.Fatal(err)
 	}
 
+	_ = pods
 	_ = input
 
 	ans := 0
