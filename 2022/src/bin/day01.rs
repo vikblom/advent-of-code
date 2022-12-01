590e860bfb62886a721f2a18e02dec3b31b04d1a
Viktor Blomqvist <veblomqvist@gmail.com>
2022-12-01 12:26:55 +0100

    2022-01 pure iterators.

--- 2022/src/bin/day01.rs
+++ 2022/src/bin/day01.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 const _INPUT: &str = include_str!("../../data/input_01.txt");
 
 const _TEST: &str = "1000
@@ -16,60 +18,25 @@ const _TEST: &str = "1000
 10000";
 
 fn part_one(input: &str) -> usize {
-    let mut max = 0;
-    // for {
-
-    // }
-    let mut sofar = 0;
-    let mut lines = input.lines().peekable();
-    while let Some(l) = lines.next() {
-        //println!("{:?} ({:?})", l, max);
-        if l.len() == 0 {
-            if sofar > max {
-                max = sofar;
-            }
-            sofar = 0;
-        } else {
-            sofar += l.parse::<usize>().unwrap()
-        }
-        // println!("state: {:?} ({:?})", l, max);
-    }
-
-    // while let i = lines
-    //     .take_while(|l| l.len() > 0)
-    //     .map(|i| i.parse::<usize>()
-    //     .sum::<usize>()
-    // {
-    //     println!("{}", i)
-    // }
-    max
+    input
+        .split("\n\n")
+        .map(|elf| elf.lines().map(|l| l.parse::<usize>().unwrap()).sum())
+        .max()
+        .unwrap()
 }
 
 fn part_two(input: &str) -> usize {
-    let mut elfs = Vec::new();
-
-    let mut sofar = 0;
-    let mut lines = input.lines().peekable();
-    while let Some(l) = lines.next() {
-        //println!("{:?} ({:?})", l, max);
-        if l.len() == 0 {
-            elfs.push(sofar);
-            sofar = 0;
-        } else {
-            sofar += l.parse::<usize>().unwrap()
-        }
-        //println!("state: {:?} ({:?})", l, max);
-    }
-    elfs.push(sofar);
-
-    elfs.sort_by(|a, b| b.cmp(a));
-    // println!("{:?}", elfs);
-    elfs.iter().take(3).sum()
+    input
+        .split("\n\n")
+        .map(|elf| elf.lines().map(|l| l.parse::<usize>().unwrap()).sum())
+        .sorted_by(|a: &usize, b: &usize| b.cmp(a))
+        .take(3)
+        .sum()
 }
 
 fn main() {
     println!("part 1 test: {}", part_one(_TEST));
-    // println!("part 1 input: {}", part_one(_INPUT));
+    println!("part 1 input: {}", part_one(_INPUT));
 
     println!("part 2 test: {}", part_two(_TEST));
     println!("part 2 input: {}", part_two(_INPUT));
