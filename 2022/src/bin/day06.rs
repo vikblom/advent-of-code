444d8853308387a53f9c7e41636f5184ebdea2c8
Viktor Blomqvist <veblomqvist@gmail.com>
2022-12-06 19:04:19 +0100

    2022-06, golfed.

--- 2022/src/bin/day06.rs
+++ 2022/src/bin/day06.rs
@@ -5,23 +5,21 @@ const _INPUT: &str = include_str!("../../data/input_06.txt");
 const _TEST: &str = "bvwbjplbgvbhsrlpgdmjqwftvncz";
 
 fn part_one(input: &str) -> usize {
-    for (i, v) in input.chars().collect::<Vec<_>>().windows(4).enumerate() {
-        let set: HashSet<_> = v.into_iter().collect();
-        if set.len() == 4 {
-            return 4 + i;
-        };
-    }
-    0
+    input
+        .as_bytes()
+        .windows(4)
+        .position(|v| HashSet::<_>::from_iter(v).len() == 4)
+        .unwrap()
+        + 4
 }
 
 fn part_two(input: &str) -> usize {
-    for (i, v) in input.chars().collect::<Vec<_>>().windows(14).enumerate() {
-        let set: HashSet<_> = v.into_iter().collect();
-        if set.len() == 14 {
-            return 14 + i;
-        };
-    }
-    0
+    input
+        .as_bytes()
+        .windows(14)
+        .position(|v| HashSet::<_>::from_iter(v).len() == 14)
+        .unwrap()
+        + 14
 }
 
 fn main() {
