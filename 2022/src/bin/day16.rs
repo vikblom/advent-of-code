5b249daa4521fe3dcb8d6ea009841da517282551
Viktor Blomqvist <veblomqvist@gmail.com>
2022-12-18 18:12:45 +0100

    2022-16, memoized.

--- 2022/src/bin/day16.rs
+++ 2022/src/bin/day16.rs
@@ -1,6 +1,13 @@
+#![feature(test)]
+
 use itertools::Itertools;
 use regex::Regex;
-use std::collections::{BTreeSet, HashMap, HashSet, VecDeque};
+use std::{
+    collections::{BTreeSet, HashMap, HashSet, VecDeque},
+    time::Instant,
+};
+
+extern crate test;
 
 const _INPUT: &str = include_str!("../../data/input_16.txt");
 
@@ -106,6 +113,43 @@ fn reachability(
     reach
 }
 
+// Compacts the neighbours into shortest path between each non-zero-flow node
+// and AA since that is the start.
+fn reachability2(
+    nbrs: &HashMap<u32, Vec<u32>>,
+    flow: &HashMap<u32, i32>,
+) -> HashMap<(u32, u32), i32> {
+    let mut reach = HashMap::new();
+
+    // Distance from each interesting node...
+    for (from, _) in nbrs {
+        if *from != pack(&"AA") && *flow.get(from).unwrap() == 0 {
+            continue;
+        }
+
+        // ... to every other interesting node.
+        // BFS - every step takes 1 minute.
+        let mut seen = HashSet::new();
+        let mut next = VecDeque::new();
+        next.push_front((from, 0));
+        while let Some((to, dist)) = next.pop_front() {
+            if seen.contains(to) {
+                continue;
+            }
+            seen.insert(to);
+
+            if *flow.get(to).unwrap() != 0 && *to != *from {
+                reach.insert((*from, *to), dist);
+            }
+
+            for n in nbrs.get(to).unwrap() {
+                next.push_back((n, dist + 1));
+            }
+        }
+    }
+    reach
+}
+
 // Exhaustive DFS to find the most pressure released, going through the reach(-able) set.
 fn dfs(maxtime: i32, flow: &HashMap<u32, i32>, reach: &HashMap<u32, Vec<(u32, i32)>>) -> i64 {
     let mut next = VecDeque::new();
@@ -147,15 +191,86 @@ fn dfs(maxtime: i32, flow: &HashMap<u32, i32>, reach: &HashMap<u32, Vec<(u32, i3
     optimum as i64
 }
 
+struct Solver {
+    reach: HashMap<(u32, u32), i32>, // (pos1, pos2) -> minutes of travel.
+    flow: HashMap<u32, i32>,         // flow if pos opened.
+    maxtime: i32,
+
+    // Memoize: time spent, current loc, remaining valves.
+    memory: HashMap<(i32, u32, Vec<u32>), i64>,
+}
+
+impl Solver {
+    // Exhaustive DFS to find the most pressure released, going through the reach(-able) set.
+
+    fn dfs(&mut self) -> i64 {
+        self.recur(
+            pack(&"AA"),
+            self.reach
+                .keys()
+                .map(|&v| v.0)
+                .unique()
+                .filter(|&v| v != pack("AA"))
+                .collect(),
+            0,
+        )
+    }
+
+    fn recur(&mut self, loc: u32, remaining: Vec<u32>, time: i32) -> i64 {
+        if let Some(&hit) = self.memory.get(&(time, loc, remaining.clone())) {
+            return hit;
+        }
+
+        let mut max = 0;
+        for (i, &to) in remaining.iter().enumerate() {
+            let time = time + 1 + self.reach.get(&(loc, to)).unwrap();
+            if time > self.maxtime {
+                continue;
+            }
+            // calc how much this vent would contribute to the total.
+            let time_active = self.maxtime - time;
+            let pressure = (time_active * self.flow.get(&to).unwrap()) as i64;
+
+            let mut remaining = remaining.clone();
+            // Needs to be remove to keep the order, since the memoization is sensitive to order.
+            remaining.remove(i);
+
+            let n = pressure + self.recur(to, remaining, time);
+
+            max = std::cmp::max(max, n);
+        }
+
+        self.memory.insert((time, loc, remaining), max);
+        max
+    }
+}
+
 fn part_one(input: &str) -> i64 {
     let (flow, neighbours) = parse(input);
     let reach = reachability(&neighbours, &flow);
 
     let mut best = 0;
+
+    let start = Instant::now();
     let n = dfs(30, &flow, &reach);
-    if n > best {
+    if dbg!(n) > best {
         best = n;
     }
+    println!("time: {:.2?}", start.elapsed());
+
+    let start = Instant::now();
+    let reach = reachability2(&neighbours, &flow);
+    let mut solver = Solver {
+        reach,
+        flow,
+        maxtime: 30,
+        memory: HashMap::new(),
+    };
+    let n = solver.dfs();
+    if dbg!(n) > best {
+        best = n;
+    }
+    println!("time: {:.2?}", start.elapsed());
     best as i64
 }
 
@@ -181,25 +296,43 @@ fn prune(keep: &Vec<u32>, old: &HashMap<u32, Vec<(u32, i32)>>) -> HashMap<u32, V
 
 fn part_two(input: &str) -> i64 {
     let (flow, neighbours) = parse(input);
-    let reach = reachability(&neighbours, &flow);
-    let keys = reach.keys().map(|&v| v).collect::<Vec<u32>>();
+    let reach = reachability2(&neighbours, &flow);
+    let keys = reach
+        .keys()
+        .map(|&v| v.0)
+        .filter(|v| *v != pack("AA"))
+        .unique()
+        .collect::<Vec<u32>>();
+    let mut solver = Solver {
+        reach,
+        flow,
+        maxtime: 26,
+        memory: HashMap::new(),
+    };
 
+    let start = Instant::now();
     // FIXME: Iterator hell, itertools refs the u32s.
     let mut best = 0;
     for n in 1..=(keys.len() / 2) {
         for combination in keys.clone().into_iter().combinations(n) {
-            let (left, right) = keys
+            let (mut left, mut right): (Vec<_>, Vec<_>) = keys
                 .clone()
                 .into_iter()
                 .partition(|k| combination.contains(k));
 
-            let me = dfs(26, &flow, &prune(&left, &reach));
-            let elephant = dfs(26, &flow, &prune(&right, &reach));
+            left.sort();
+            right.sort();
+
+            let me = solver.recur(pack(&"AA"), left, 0);
+            let elephant = solver.recur(pack(&"AA"), right, 0);
+            // let me = dfs(26, &flow, &prune(&left, &reach));
+            // let elephant = dfs(26, &flow, &prune(&right, &reach));
             if me + elephant > best {
                 best = me + elephant;
             }
         }
     }
+    println!("time: {:.2?}", start.elapsed());
 
     best as i64
 }
@@ -210,4 +343,15 @@ fn main() {
 
     println!("part 2 test: {}", part_two(_TEST));
     println!("part 2 input: {}", part_two(_INPUT));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test::Bencher;
+
+    #[bench]
+    fn bench_part_one(b: &mut Bencher) {
+        b.iter(|| part_one(_INPUT));
+    }
 }
