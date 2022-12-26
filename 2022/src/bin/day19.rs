bf3c427b3b596163534123f320385ad8e2d7d2d1
Viktor Blomqvist <veblomqvist@gmail.com>
2022-12-26 11:53:28 +0100

    2022-19, optimized.

--- 2022/src/bin/day19.rs
+++ 2022/src/bin/day19.rs
@@ -1,4 +1,4 @@
-use std::{collections::BTreeMap, time::Instant};
+use std::time::Instant;
 
 use regex::Regex;
 
@@ -38,13 +38,13 @@ fn parse(input: &str) -> Vec<Machine> {
     machines
 }
 
-#[derive(Debug, Default, Clone, Hash, Ord, Eq, PartialEq, PartialOrd)]
+#[derive(Debug)]
 struct State {
     // Derive PartialOrd that sorts on states we want.
-    geo: i32,
-    obs: i32,
-    clay: i32,
     ore: i32,
+    clay: i32,
+    obs: i32,
+    geo: i32,
 
     time: i32,
 
@@ -54,105 +54,125 @@ struct State {
     geo_bots: i32,
 }
 
-fn alt(machine: &Machine, maxtime: i32) -> i32 {
-    let mut queue = BTreeMap::new();
+fn div_up(a: i32, b: i32) -> i32 {
+    (a + (b - 1)) / b
+}
 
-    queue.insert(
-        State {
-            time: 0,
-            ore: 0,
-            clay: 0,
-            obs: 0,
-            geo: 0,
-            ore_bots: 1,
-            clay_bots: 0,
-            obs_bots: 0,
-            geo_bots: 0,
-        },
-        (),
-    );
+fn maximize_geodes(machine: &Machine, maxtime: i32) -> i32 {
+    let mut queue = Vec::new();
+
+    queue.push(State {
+        time: 0,
+        ore: 0,
+        clay: 0,
+        obs: 0,
+        geo: 0,
+        ore_bots: 1,
+        clay_bots: 0,
+        obs_bots: 0,
+        geo_bots: 0,
+    });
 
     let max_ore_bots = machine.ore_ore.max(machine.clay_ore).max(machine.obs_ore);
     let max_clay_bots = machine.obs_clay;
     let max_obs_bots = machine.geo_obs;
 
     let mut best = 0;
-    while let Some((state, _)) = queue.pop_first() {
-        if state.geo > best {
-            best = state.geo;
+    while let Some(state) = queue.pop() {
+        let guaranteed = state.geo + state.geo_bots * (maxtime - state.time);
+        if guaranteed > best {
+            best = guaranteed;
         }
         if state.time == maxtime {
             continue;
         }
 
-        // If we have zero geo bots, and it would cost more than we can get, just skip.
+        // Is it even possible to beat the best?
         let left = maxtime - state.time;
-        if state.geo_bots == 0 && (left * (left - 1)) / 2 + state.obs < machine.geo_obs {
+        if state.geo + state.geo_bots * left + (left * (left - 1)) / 2 < best {
             continue;
         }
 
-        // This is what we get next timestep as a base.
-        let next = State {
-            time: state.time + 1,
-            ore: state.ore + state.ore_bots,
-            clay: state.clay + state.clay_bots,
-            obs: state.obs + state.obs_bots,
-            geo: state.geo + state.geo_bots,
-            ..state
-        };
-        // We could use resources to build a bot.
-        // Greedily build a geo bot if possible, and if so then only that bot.
-        if state.obs >= machine.geo_obs && state.ore >= machine.geo_ore {
-            queue.insert(
-                State {
-                    ore: state.ore + state.ore_bots - machine.geo_ore,
-                    obs: state.obs + state.obs_bots - machine.geo_obs,
-                    geo_bots: state.geo_bots + 1,
-                    ..next
-                },
-                (),
-            );
-        } else {
-            // Obsidian bot
-            if state.clay >= machine.obs_clay
-                && state.ore >= machine.obs_ore
-                && state.obs_bots < max_obs_bots
-            {
-                queue.insert(
-                    State {
-                        ore: state.ore + state.ore_bots - machine.obs_ore,
-                        clay: state.clay + state.clay_bots - machine.obs_clay,
-                        obs_bots: state.obs_bots + 1,
-                        ..next
-                    },
-                    (),
-                );
-            }
-            // Clay bot
-            if state.ore >= machine.clay_ore && state.clay_bots < max_clay_bots {
-                queue.insert(
-                    State {
-                        ore: state.ore + state.ore_bots - machine.clay_ore,
-                        clay_bots: state.clay_bots + 1,
-                        ..next
-                    },
-                    (),
-                );
-            }
-            // Ore bot
-            if state.ore >= machine.ore_ore && state.ore_bots < max_ore_bots {
-                queue.insert(
-                    State {
-                        ore: state.ore + state.ore_bots - machine.ore_ore,
-                        ore_bots: state.ore_bots + 1,
-                        ..next
-                    },
-                    (),
-                );
-            }
+        // Instead of moving one minute in each step, building when possible,
+        // pick one thing to build and go directly to the time at which it would have
+        // been built. Requires some fiddling and picking the limiting resource.
+        // Plus 1 minute for the actual build.
 
-            // Or we just have to wait.
-            queue.insert(next, ());
+        // build ore bot.
+        let ttb = 1 + div_up((machine.ore_ore - state.ore).max(0), state.ore_bots);
+        if state.ore_bots < max_ore_bots && state.time + ttb <= maxtime {
+            queue.push(State {
+                time: state.time + ttb,
+                ore: state.ore + ttb * state.ore_bots - machine.ore_ore,
+                ore_bots: state.ore_bots + 1,
+
+                clay: state.clay + ttb * state.clay_bots,
+                obs: state.obs + ttb * state.obs_bots,
+                geo: state.geo + ttb * state.geo_bots,
+                // copy other bots
+                ..state
+            });
+        }
+
+        // build clay bot
+        let ttb = 1 + div_up((machine.clay_ore - state.ore).max(0), state.ore_bots);
+        if state.clay_bots < max_clay_bots && state.time + ttb <= maxtime {
+            queue.push(State {
+                time: state.time + ttb,
+                ore: state.ore + ttb * state.ore_bots - machine.clay_ore,
+                clay_bots: state.clay_bots + 1,
+
+                clay: state.clay + ttb * state.clay_bots,
+                obs: state.obs + ttb * state.obs_bots,
+                geo: state.geo + ttb * state.geo_bots,
+                // copy other bots
+                ..state
+            });
+        }
+
+        if state.clay_bots == 0 {
+            // Not even possible to build more advanced bots.
+            continue;
+        }
+
+        // build obs bot
+        let ttb = 1 + std::cmp::max(
+            div_up((machine.obs_ore - state.ore).max(0), state.ore_bots),
+            div_up((machine.obs_clay - state.clay).max(0), state.clay_bots),
+        );
+        if state.obs_bots < max_obs_bots && state.time + ttb <= maxtime {
+            queue.push(State {
+                time: state.time + ttb,
+                ore: state.ore + ttb * state.ore_bots - machine.obs_ore,
+                clay: state.clay + ttb * state.clay_bots - machine.obs_clay,
+                obs_bots: state.obs_bots + 1,
+
+                obs: state.obs + ttb * state.obs_bots,
+                geo: state.geo + ttb * state.geo_bots,
+                // copy other bots
+                ..state
+            });
+        }
+
+        if state.obs_bots > 0 {
+            // build geo bot
+            let ttb = 1 + std::cmp::max(
+                div_up((machine.geo_ore - state.ore).max(0), state.ore_bots),
+                div_up((machine.geo_obs - state.obs).max(0), state.obs_bots),
+            );
+            if state.time + ttb <= maxtime {
+                queue.push(State {
+                    time: state.time + ttb,
+                    ore: state.ore + ttb * state.ore_bots - machine.geo_ore,
+                    obs: state.obs + ttb * state.obs_bots - machine.geo_obs,
+                    geo_bots: state.geo_bots + 1,
+
+                    clay: state.clay + ttb * state.clay_bots,
+                    geo: state.geo + ttb * state.geo_bots,
+                    // copy other bots
+                    ..state
+                });
+            }
         }
     }
 
@@ -162,7 +182,7 @@ fn alt(machine: &Machine, maxtime: i32) -> i32 {
 fn part_one_alt(input: &str) -> i64 {
     let mut sum = 0;
     for m in parse(input) {
-        let quality = alt(&m, 24);
+        let quality = maximize_geodes(&m, 24);
         sum += m.id * quality;
         println!("machine: {} quality {}", m.id, quality);
     }
@@ -172,32 +192,19 @@ fn part_one_alt(input: &str) -> i64 {
 fn part_two_alt(input: &str) -> i64 {
     let mut prod = 1;
     for m in parse(input).iter().take(3) {
-        let best = alt(&m, 32);
+        let best = maximize_geodes(&m, 32);
         prod *= best as i64;
         println!("machine: {} best: {}", m.id, best);
     }
-
     prod as i64
 }
 
 fn main() {
-    // println!("part 1 test: {}", part_one(_TEST));
-    // let start = Instant::now();
-    // let p1 = part_one(_INPUT);
-    // let dur = start.elapsed();
-    // println!("part 1 input: {} ({:.2?})", p1, dur);
-
     let start = Instant::now();
     let p1 = part_one_alt(_INPUT);
     let dur = start.elapsed();
     println!("alt 1 input: {} ({:.2?})", p1, dur);
 
-    // println!("part 2 test: {}", part_two(_TEST));
-
-    // let start = Instant::now();
-    // let p2 = part_two(_INPUT);
-    // let dur = start.elapsed();
-    // println!("part 2 input: {} ({:.2?})", p2, dur);
     let start = Instant::now();
     let p2 = part_two_alt(_INPUT);
     let dur = start.elapsed();
