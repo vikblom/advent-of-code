ede3a5d8d6d120f0848f8740a63bdda73ae7c728
Viktor Blomqvist <veblomqvist@gmail.com>
2023-12-05 21:49:37 +0100

    2023-05, non-brute solution for part 2.

--- 2023/05/solve_test.go
+++ 2023/05/solve_test.go
@@ -69,6 +69,8 @@ func TestPartOne(t *testing.T) {
 }
 
 func TestPartTwo(t *testing.T) {
+	t.Skip("Replaced")
+
 	_, header, _ := strings.Cut(inputLines[0], ": ")
 	seeds := []int{}
 	for _, s := range strings.Split(header, " ") {
@@ -119,4 +121,113 @@ func TestPartTwo(t *testing.T) {
 		ans = min(ans, v)
 	}
 	aoc.Answer(ans, 7873084)
+}
+
+type Interval struct {
+	start, end, x int
+}
+
+func TestPartTwoRanges(t *testing.T) {
+	_, header, _ := strings.Cut(inputLines[0], ": ")
+	raw := strings.Split(header, " ")
+	seeds := []Interval{}
+	for i := 0; i < len(raw); i += 2 {
+		a := aoc.MustInt(raw[i])
+		r := aoc.MustInt(raw[i+1])
+		seeds = append(seeds, Interval{
+			start: a,
+			end:   a + r,
+		})
+	}
+
+	chain := [][]Interval{}
+	this := []Interval{}
+	for _, block := range strings.Split(strings.TrimRight(input, "\n"), "\n\n")[1:] {
+		lines := strings.Split(block, "\n")
+		for _, l := range lines[1:] {
+			nums := strings.Split(l, " ")
+			ints := []int{}
+			for _, n := range nums {
+				ints = append(ints, aoc.MustInt(n))
+			}
+			dest, src, length := ints[0], ints[1], ints[2]
+			this = append(this, Interval{start: src, end: src + length, x: dest - src})
+		}
+
+		slices.SortFunc(this, func(a, b Interval) int { return a.start - b.start })
+		chain = append(chain, this)
+		this = []Interval{}
+	}
+
+	for _, maps := range chain {
+		next := []Interval{}
+	next:
+		for i := 0; i < len(seeds); i++ {
+			a := seeds[i].start
+			b := seeds[i].end
+			for _, m := range maps {
+				c := m.start
+				d := m.end
+				// fmt.Printf("[%d %d)  over [%d %d) + %d\n", a, b, c, d, m.x)
+				switch {
+				case d <= a:
+					// Keep looking, maps will be bigger and bigger.
+					//      a b
+					// c d
+					continue
+				case b <= c:
+					// Went past, keep the same.
+					// a b              => [a b)
+					//     c d
+					next = append(next, Interval{
+						start: a,
+						end:   b,
+					})
+					continue next
+				case c <= a && b <= d:
+					//   a b            => [a b)+x
+					// c     d
+					next = append(next, Interval{
+						start: a + m.x,
+						end:   b + m.x,
+					})
+					continue next
+				case c <= b && b < d:
+					// a   b           => [a c) [c, b)+x
+					//   c   d
+					next = append(next, Interval{
+						start: c + m.x,
+						end:   b + m.x,
+					})
+					// [a c) does not carry since it has already been passed.
+					continue next
+				case a < d && d < b:
+					//   a   b          => [a d)+x [d b)
+					// c   d
+					next = append(next, Interval{
+						start: a + m.x,
+						end:   d + m.x,
+					})
+					if d < b {
+						seeds = append(seeds, Interval{
+							start: d,
+							end:   b,
+						})
+					}
+					continue next
+
+				default:
+					panic("unreachable")
+				}
+			}
+			next = append(next, Interval{
+				start: a,
+				end:   b,
+			})
+		}
+		seeds = next[:]
+	}
+
+	slices.SortFunc(seeds, func(a, b Interval) int { return a.start - b.start })
+	aoc.Answer(seeds[0].start, 7873084)
 }
