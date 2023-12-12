2b10c98c409a65d333081ac289c673fc0c0b5a2d
Viktor Blomqvist <veblomqvist@gmail.com>
2023-12-12 18:25:59 +0100

    2023-12, optmized memoization a bit.

--- 2023/12/solve_test.go
+++ 2023/12/solve_test.go
@@ -1,7 +1,9 @@
 package solve
 
 import (
+	"encoding/binary"
 	"fmt"
+	"hash/fnv"
 	"strings"
 	"testing"
 
@@ -26,11 +28,34 @@ func fits(s string, size int) (ret bool) {
 	return (size == len(s)) || s[size] != '#'
 }
 
-var mem = map[string]int{}
+var (
+	hasher = fnv.New64()
+	buf    = make([]byte, 0, 512)
+)
+
+// Go cannot hash a slice, and building strings as keys
+// is expensive, so we hash it ourselves.
+// This will however be hashed again by the map.
+func hash(s string, vs []int) uint64 {
+	defer hasher.Reset()
+	defer func() {
+		hasher.Reset()
+		buf = buf[:0]
+	}()
+
+	buf = append(buf, []byte(s)...)
+	for _, v := range vs {
+		buf = binary.BigEndian.AppendUint64(buf, uint64(v))
+	}
+	hasher.Write(buf)
+	return hasher.Sum64()
+}
+
+var mem = map[uint64]int{}
 
 func combos(springs string, groups []int) (ret int) {
 	// Memoized.
-	key := fmt.Sprintf("%s%v", springs, groups)
+	key := hash(springs, groups)
 	if ans, ok := mem[key]; ok {
 		return ans
 	}
